8479a7dc4fdb07794ad6b488039d50a8fc0b2e55
Shaun <{ID}+{username}@users.noreply.github.com>
2025-05-02 11:33:55 +0800

    move to script

---
+++ script/script.js
@@ -0,0 +1,168 @@
+const dataAPI = "https://datacenter.taichung.gov.tw/swagger/OpenData/c923ad20-2ec6-43b9-b3ab-54527e99f7bc";
+var curlat, curlng, fylat, fylng, Mapdata, geo = 0;
+
+fylat = 24.2543403;
+fylng = 120.7226995;
+
+$(function () {
+    navigator.geolocation.getCurrentPosition(success, fail, { maximumAge: 500000, enableHighAccuracy: true, timeout: 6000 });
+
+    if (navigator.geolocation) {
+        success();
+    } else {
+        fail();
+    }
+});
+
+function success(position) {
+
+    curlat = position.coords.latitude;
+    curlng = position.coords.longitude;
+    Mapdata = initMap(curlat, curlng);
+
+    $.ajax({
+        type: "GET",
+        url: dataAPI,
+        dataType: "json",
+        success: function () {
+            let data = {};
+            data.latitude = curlat;
+            data.longitude = curlng;
+            data.map = Mapdata.map;
+            data.markers = Mapdata.markers;
+
+            show(data);
+
+            const coordinatesMap = {
+                curlat: curlat,
+                curlng: curlng,
+                Mapdata: Mapdata
+            };
+            reFreshPage(coordinatesMap);
+        },
+        error: function () {
+            alert("opendata error");
+        }
+    });
+}
+
+function fail(error) {
+
+    $.ajax({
+        type: "GET",
+        url: dataAPI,
+        dataType: "json",
+        success: function () {
+            const locfailed = locateFailed();
+            reFreshPage(locfailed);
+        },
+        error: function () {
+            alert("opendata error");
+        }
+    });
+}
+
+function locateFailed() {
+    let data = {};
+
+    Mapdata = initMap(fylat, fylng);
+
+    data.latitude = fylat;
+    data.longitude = fylng;
+    data.map = Mapdata.map;
+    data.markers = Mapdata.markers;
+
+    show(data);
+
+    return { curlat: fylat, curlng: fylng, Mapdata: Mapdata };
+}
+
+async function reFreshPage(coordinatesMap) {
+    var contentDiv = document.getElementById('map');
+
+    async function fetchData() {
+        try {
+            const response = await fetch(dataAPI);
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+            const map = coordinatesMap.Mapdata.map;
+            const markers = coordinatesMap.Mapdata.markers;
+            const curlat = coordinatesMap.curlat;
+            const curlng = coordinatesMap.curlng;
+
+            var blueIcon = new L.Icon({
+                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+                shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                shadowSize: [41, 41]
+            });
+
+            for (var i = 0; i < data.length; i++) {
+                markers.addLayer(L.marker([data[i].Y, data[i].X], { icon: blueIcon }).bindPopup('<div class="card"><div class="card-head"><h5 class="card-title">' + data[i].car + '</h5></div><div class="card-body"><p>車號：' + data[i].car + '</p><p>地點：' + data[i].location + '</p><p>更新時間：' + data[i].time + '</p></div></div>'));
+            }
+
+            map.addLayer(markers);
+        } catch (error) {
+            console.error('Fetch error:', error);
+            contentDiv.textContent = 'Failed to load data';
+        }
+    }
+
+    fetchData();
+
+    setInterval(fetchData, 60000);
+}
+
+function show(data) {
+
+    var goldIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
+    var blueIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
+    var map = data.map;
+    var markers = data.markers;
+
+    markers.addLayer(L.marker([data.latitude, data.longitude], { icon: goldIcon }).bindPopup("定位完成!"));
+
+    for (var i = 0; i < data.length; i++) {
+        markers.addLayer(L.marker([data[i].Y, data[i].X], { icon: blueIcon }).bindPopup('<div class="card"><div class="card-head"><h5 class="card-title">' + data[i].car + '</h5></div><div class="card-body"><p>車號：' + data[i].car + '</p><p>地點：' + data[i].location + '</p><p>更新時間：' + data[i].time + '</p></div></div>'));
+    }
+
+    map.addLayer(markers);
+}
+
+function initMap(lat, lng) {
+
+    var map = L.map('map').setView([lat, lng], 17);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var markers = L.markerClusterGroup().addTo(map);
+    map.addLayer(markers);
+
+    var initmap = { map: map, markers: markers };
+
+    return initmap;
+}
