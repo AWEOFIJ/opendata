23d83c81e997eb0d5b0971fe8dfcdb30706ae990
Shaun <{ID}+{username}@users.noreply.github.com>
2025-05-06 10:33:10 +0800

    comment

--- script/script.js
+++ script/script.js
@@ -1,5 +1,4 @@
 const dataAPI = "https://datacenter.taichung.gov.tw/swagger/OpenData/c923ad20-2ec6-43b9-b3ab-54527e99f7bc";
-// const testAPI = "https://datacenter.taichung.gov.tw/swagger/OpenData/6bfd1b96-2435-4ce0-a8aa-418eda9e8f02";
 var curlat, curlng, fylat, fylng, Mapdata, map;
 
 fylat = 24.2543403;
@@ -7,22 +6,7 @@ fylng = 120.7226995;
 
 $(function () {
     navigator.geolocation.getCurrentPosition(success, fail, { maximumAge: 500000, enableHighAccuracy: true, timeout: 6000 });
-    // goTestAPI();
 });
-
-// function goTestAPI() { 
-//     $.ajax({
-//         type: "GET",
-//         url: testAPI,
-//         dataType: "json",
-//         success: function (data) {
-//             console.log(data);
-//         },
-//         error: function () {
-//             alert("opendata error");
-//         }
-//     });
-// }
 
 function success(position) {
 
