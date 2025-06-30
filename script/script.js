3a46b815b3a2176104023809b7ca116ba9ae20d8
ShaunHsieh <shaunhsieh890@gmail.com>
2025-07-01 00:20:30 +0800

    timeStamp only to UTS is certain. This is prototype.

--- script/script.js
+++ script/script.js
@@ -6,23 +6,12 @@ fylng = 120.7226995;
 
 $(function () {
 
-    // Get the current timestamp
-    // Convert the timestamp to UTC string format
-    
-    const time = Date.now();
-    console.log("Current timeStamp: /* Format: YYYYMMDDTTHHMMSS */ ", time.toUTCString());
-
-    const now = new Date();
-    
-    let timestamp = now.toISOString().replace(/[-:]/g, '').split('.')[0]; // Format: YYYYMMDDTHHMMSS
-    timestamp = timestamp.replace('T', ''); // Remove the 'T' to get YYYYMMDDHHMMSS format
-    console.log("Current timestamp: /* (ISO time) Format: YYYYMMDDTTHHMMSS */ ", timestamp);
-    
-    // 監聽定位變化
     alert("請允許瀏覽器定位功能！");
+    
+    const time = new Date();
 
-    // navigator.geolocation.getCurrentPosition(success, fail, { maximumAge: 60000, enableHighAccuracy: true, timeout: 6000 });
-    // 使用 watchPosition 來持續監聽位置變化
+    let timeStamp = time.toUTCString();
+    console.log("(This is UTC and only UTC.) Current timeStamp: ", timeStamp);
 
     navigator.geolocation.watchPosition(success, fail, { maximumAge: 60000, enableHighAccuracy: true, timeout: 6000 });
 });
@@ -109,50 +98,6 @@ function show(data) {
     }
 
     map.addLayer(markers);
-
-    // setTimeout(() => {
-    //     reFreshPage(data);
-    // }, 30000); // 預設30秒後刷新一次而且只有一次
-
-}
-
-function reFreshPage(data) {
-
-    setInterval(() => {
-
-        const map = data.map;
-        const markers = data.markers;
-        const curlat = data.curlat;
-        const curlng = data.curlng;
-
-        $.ajax({
-            type: "GET",
-            url: dataAPI,
-            dataType: "json",
-            success: function (data) {
-
-                markers.clearLayers();
-
-                var blueIcon = new L.Icon({
-                    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
-                    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-                    iconSize: [25, 41],
-                    iconAnchor: [12, 41],
-                    popupAnchor: [1, -34],
-                    shadowSize: [41, 41]
-                });
-
-                for (var i = 0; i < data.length; i++) {
-                    markers.addLayer(L.marker([data[i].Y, data[i].X], { icon: blueIcon }).bindPopup('<div class="card"><div class="card-head"><h5 class="card-title">' + data[i].car + '</h5></div><div class="card-body"><p>車號：' + data[i].car + '</p><p>地點：' + data[i].location + '</p><p>更新時間：' + data[i].time + '</p></div></div>'));
-                }
-
-                map.addLayer(markers);
-            },
-            error: function () {
-                alert("opendata error");
-            }
-        });
-    }, 60000); // 每60秒刷新一次
 }
 
 function initMap(lat, lng) {
