81af95f20769de47b7091fa3248893a8f4639b39
Shaun Hsieh <shaunhsieh890@gmail.com>
2025-10-06 20:22:49 +0800

    redIcon_work

--- script/script.js
+++ script/script.js
@@ -16,6 +16,14 @@ var goldIcon = new L.Icon({
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
 });
+var redIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
 var blueIcon = new L.Icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
@@ -43,7 +51,7 @@ function initMap(lat, lng) {
         });
         userMarkers = L.markerClusterGroup().addTo(map);
 
-        userMarkers.addLayer(L.marker([lat, lng], { icon: goldIcon }).bindPopup("定位完成!"));
+        userMarkers.addLayer(L.marker([lat, lng], { icon: redIcon }).bindPopup("定位完成!"));
 
     } else {
         map.setView([lat, lng], 17);
@@ -121,11 +129,13 @@ function getLocation(position) {
     curlng = position.coords.longitude;
 
     initMap(curlat, curlng);
+    loadData();
     startAutoRefresh();
 }
 
 function defaultloCation() {
     initMap(fylat, fylng);
+    loadData();
     startAutoRefresh();
 }
 
